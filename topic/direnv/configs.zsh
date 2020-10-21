b8df9c5fdc8bfa7d5c12fb40074d825bce43318f
Raghavendra Bhuvan <rage28@gmail.com>
2020-10-21 13:52:58 +0530

    Use a config for direnv

---
+++ topic/direnv/configs.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+exists direnv && {
+    eval "$(direnv hook zsh)"
+}
--- topic/direnv/plugins.zsh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-
-zplugin ice as"program" make"!" atclone"./direnv hook zsh > zhook.zsh" atpull"%atclone" src"zhook.zsh"
-zplugin light "direnv/direnv"
