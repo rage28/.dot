83e639f747ca55a36e7d9fdea5a75c03659dcb58
Raghavendra Bhuvan <rage28@gmail.com>
2023-04-10 16:39:17 +0530

    Fix zoxide completion branch

--- topic/zoxide/completion.zsh
+++ topic/zoxide/completion.zsh
@@ -2,7 +2,7 @@
 
 exists zoxide && {
     zinit ice as"completion" lucid wait"1" atinit"zpcompinit" mv"completion -> _z"
-    zinit snippet https://github.com/rage28/.dot/blob/feature/zoxide/topic/zoxide/completion
+    zinit snippet https://github.com/rage28/.dot/blob/master/topic/zoxide/completion
 
     eval "$(zoxide init zsh)"
 }
