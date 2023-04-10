f1eeb2da6ff763a61cca70e9ca4de1782dfd8fbb
Raghavendra Bhuvan <rage28@gmail.com>
2023-04-10 16:38:45 +0530

    Switch to material LS_COLORS

--- topic/lscolors/plugins.zsh
+++ topic/lscolors/plugins.zsh
@@ -2,5 +2,5 @@
 
 # For GNU ls (the binaries can be gls, gdircolors, e.g. on OS X when installing the
 # coreutils package from Homebrew; you can also use https://github.com/ogham/exa)
-zinit ice atclone"dircolors -b LS_COLORS > c.zsh" atpull'%atclone' pick"c.zsh" nocompile'!'
-zinit light trapd00r/LS_COLORS
+zinit ice wait lucid
+zinit light zpm-zsh/material-colors
