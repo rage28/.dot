a1f6afe1d852d8272ad876f9321fd24b0df48e4f
Raghavendra Bhuvan <rage28@gmail.com>
2023-04-17 10:59:02 +0530

    Revert zellij use

--- topic/tmux/install.sh
+++ topic/tmux/install.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
-# exists tmux || {
-#   fail "tmux not installed. Please visit https://github.com/tmux/tmux/wiki/Installing"
-# }
+exists tmux || {
+  fail "tmux not installed. Please visit https://github.com/tmux/tmux/wiki/Installing"
+}
 
-# exists tmux && {
-#   TPM_SOURCE="${TOPICS}/tmux/tpm"
-#   TPM_TARGET="$HOME/.tmux/plugins"
+exists tmux && {
+  TPM_SOURCE="${TOPICS}/tmux/tpm"
+  TPM_TARGET="$HOME/.tmux/plugins"
 
-#   if [ -d "${TPM_TARGET}/tpm" ]; then
-#     success "tmux-plugin-manager already installed"
-#   else
-#     info "Installing tmux-plugin-manager..."
-#     if [ ! -d "${TPM_SOURCE}"/.git ]; then
-#       clone https://github.com/tmux-plugins/tpm.git "${TPM_SOURCE}"
-#     fi
+  if [ -d "${TPM_TARGET}/tpm" ]; then
+    success "tmux-plugin-manager already installed"
+  else
+    info "Installing tmux-plugin-manager..."
+    if [ ! -d "${TPM_SOURCE}"/.git ]; then
+      clone https://github.com/tmux-plugins/tpm.git "${TPM_SOURCE}"
+    fi
 
-#     overwrite_prev_line
-#     mkdir -p "${TPM_TARGET}"
-#     link "${TPM_SOURCE}" "${TPM_TARGET}" false
-#     success "Installed tmux-plugin-manager"
-#   fi
-# }
+    overwrite_prev_line
+    mkdir -p "${TPM_TARGET}"
+    link "${TPM_SOURCE}" "${TPM_TARGET}" false
+    success "Installed tmux-plugin-manager"
+  fi
+}
