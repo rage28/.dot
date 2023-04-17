154ed3284cef684bea83047dedf24890b2c02a77
Raghavendra Bhuvan <rage28@gmail.com>
2023-04-17 10:59:02 +0530

    Revert zellij use

--- topic/zellij/install.sh
+++ topic/zellij/install.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-exists zellij || {
-  fail "zellij not installed. Please visit https://zellij.dev/documentation/installation.html"
-}
+# exists zellij || {
+#   fail "zellij not installed. Please visit https://zellij.dev/documentation/installation.html"
+# }
 
-declare ZELLIJ_CONF_SOURCE="${DOTFILES_SRC}/topic/zellij/config"
-declare ZELLIJ_CONF_TARGET="${HOME}/.config/zellij"
+# declare ZELLIJ_CONF_SOURCE="${DOTFILES_SRC}/topic/zellij/config"
+# declare ZELLIJ_CONF_TARGET="${HOME}/.config/zellij"
 
-exists zellij && {
-  if [ -d "${ZELLIJ_CONF_TARGET}" ]; then
-    success "zellij is already configured"
-  else
-    notice "zellij is getting configured..."
+# exists zellij && {
+#   if [ -d "${ZELLIJ_CONF_TARGET}" ]; then
+#     success "zellij is already configured"
+#   else
+#     notice "zellij is getting configured..."
 
-    link "${ZELLIJ_CONF_SOURCE}" "${ZELLIJ_CONF_TARGET}"
+#     link "${ZELLIJ_CONF_SOURCE}" "${ZELLIJ_CONF_TARGET}"
 
-    overwrite_prev_line
-    success "zellij successfully configured" "yes"
-  fi
-}
+#     overwrite_prev_line
+#     success "zellij successfully configured" "yes"
+#   fi
+# }
