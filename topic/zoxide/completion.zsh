0b1b35f5af85e9618b40779ccda850d040fda441
Raghavendra Bhuvan <rage28@gmail.com>
2023-05-21 22:56:59 +0530

    Fix again zoxide completion

--- topic/zoxide/completion.zsh
+++ topic/zoxide/completion.zsh
@@ -1,6 +1,5 @@
 #!/usr/bin/env zsh
 
 exists zoxide && {
-    zinit ice as"completion" lucid wait"1" atinit"zpcompinit" mv"completion -> _z"
     zinit snippet https://raw.githubusercontent.com/rage28/.dot/master/topic/zoxide/completion
 }
